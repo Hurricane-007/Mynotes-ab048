e41a69382cdb6c7ebeebec07e035b6fae11dfd3a
Aryan <aryan31122004@gmail.com>
2024-12-27 00:32:11 +0530

    step 9

--- lib/views/notes/new_note.dart
+++ lib/views/notes/new_note.dart
@@ -1,4 +1,4 @@
-import 'dart:ffi';
+// import 'dart:ffi';
 
 import 'package:flutter/material.dart';
 import 'package:mynotes/services/auth/auth_service.dart';
--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -21,11 +21,6 @@ String get userEmail => AuthService.firebase().currentUser!.email!;
     _notesService = NotesService();
     super.initState();
   }
-@override
-  void dispose() {
-    _notesService.close();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -68,8 +63,25 @@ String get userEmail => AuthService.firebase().currentUser!.email!;
                 switch(snapshot.connectionState){
 
                   case ConnectionState.waiting:
-                  case ConnectionState.done:
-                    return const Text('Waiting for all notes....');
+                  case ConnectionState.active:
+                  if(snapshot.hasData){
+                    final allNotes = snapshot.data as List<DatabaseNote>;
+                    return ListView.builder(itemCount: allNotes.length,
+                    itemBuilder: (context, index) {
+                      final note = allNotes[index];
+                      return ListTile(
+                        title: Text(
+                          note.text,
+                          maxLines: 1,
+                          softWrap: true,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      );
+                    },);
+                    
+                  }else{
+                    return const CircularProgressIndicator();
+                  }
                   default:
                    return const CircularProgressIndicator();
                 }
